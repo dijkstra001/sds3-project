113452c74c0c1ae62dbc8102d1038e3483abd876
dijkstra001 <ncorreiatrabalhando@gmail.com>
2021-05-05 09:13:25 -0300

    Add static charts

--- SDS3/frontend/src/App.tsx
+++ SDS3/frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "assets/components/Barchart";
 import DataTable from "assets/components/Datatable/DataTable";
+import DonutChart from "assets/components/Donutchart";
 import Footer from "assets/components/Footer";
 import NavBar from "assets/components/Navbar";
 function App() {
@@ -6,7 +8,23 @@ function App() {
     <>
     <NavBar />
     <div className="container">
-      <h1 className="text-primary">Hello world.</h1>
+      <h1 className="text-primary py-3">Dashboard de vendas</h1>
+      <div className="row px-3">
+
+        <div className="col-sm-6 text-center text-secondary">
+          <h5>Taxa de sucesso</h5>
+          <BarChart />
+        </div>
+        <div className="col-sm-6 text-center text-secondary">
+          <h5>Todas as vendas</h5>
+          <DonutChart />
+        </div>
+
+      </div>
+
+      <div className="py-3">
+        <h2 className="text-primary">Tabela de vendas</h2>
+      </div>
       <DataTable />
     </div>
     <Footer />
