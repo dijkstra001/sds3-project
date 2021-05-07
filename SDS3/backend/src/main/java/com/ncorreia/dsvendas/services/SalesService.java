d5a427aaeb2b2c2bd4d86455d0bc469afe417b6a
dijkstra001 <ncorreiatrabalhando@gmail.com>
2021-05-07 18:02:35 -0300

    Add pagination request

--- SDS3/backend/src/main/java/com/ncorreia/dsvendas/services/SalesService.java
+++ SDS3/backend/src/main/java/com/ncorreia/dsvendas/services/SalesService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ncorreia.dsvendas.dto.SalesDto;
 import com.ncorreia.dsvendas.entities.Sales;
 import com.ncorreia.dsvendas.repositories.SalesRepository;
+import com.ncorreia.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SalesService {
@@ -15,7 +17,12 @@ public class SalesService {
 	@Autowired
 	public SalesRepository salesRepository;
 	
+	@Autowired
+	public SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SalesDto> findAll(Pageable pageable) {
+		sellerRepository.findAll();
 		Page<Sales>  sales = salesRepository.findAll(pageable);
 		return sales.map( x -> new SalesDto(x));
 	}
