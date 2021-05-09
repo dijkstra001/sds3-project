f4c63bc6bf4c02409bdb327b478a24c93d96cc3c
dijkstra001 <ncorreiatrabalhando@gmail.com>
2021-05-09 00:41:40 -0300

    Add navbar and dashboard links

--- SDS3/frontend/src/pages/Home/index.tsx
+++ SDS3/frontend/src/pages/Home/index.tsx
@@ -1,12 +1,21 @@
+import Footer from "assets/components/Footer";
+import NavBar from "assets/components/Navbar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
         <>
-            <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-                <div className="container">
-                    <h1>Página home</h1>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">Dashboard de vendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <Link className="btn btn-primary btn-large" to="/dashboard">Acessar o dashboard</Link>
                 </div>
-
             </div>
+            <Footer />
         </>
     );
 }
